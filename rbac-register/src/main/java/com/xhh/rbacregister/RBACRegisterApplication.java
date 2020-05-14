8d911f8c9b75c4a34f2602b207d2fdf063cdd964
xhh <303158131@qq.com>
2020-05-14 13:25:45 +0800

    init project

---
+++ rbac-register/src/main/java/com/xhh/rbacregister/RBACRegisterApplication.java
@@ -0,0 +1,15 @@
+package com.xhh.rbacregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class RBACRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RBACRegisterApplication.class, args);
+    }
+
+}
