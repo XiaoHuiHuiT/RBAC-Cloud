a82f6ddd4b4fa2c39ecc91d912f11ea4fde87167
xhh <303158131@qq.com>
2020-05-14 19:37:37 +0800

    调整打印样式,添加注释

--- rbac-server/rbac-server-test/src/main/java/com/xhh/rbac/server/test/RBACServerTestApplication.java
+++ rbac-server/rbac-server-test/src/main/java/com/xhh/rbac/server/test/RBACServerTestApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
+// 开启服务注册与发现
 @EnableDiscoveryClient
 @SpringBootApplication
+// 开启Spring Cloud Security权限注解
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class RBACServerTestApplication {
 
