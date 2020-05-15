d9ea7da01458f365fe01a23ba8f3ac9ae0149e5e
xhh <303158131@qq.com>
2020-05-15 10:36:04 +0800

    登录流程完善

---
+++ rbac-gateway/src/main/java/com/xhh/rbac/gateway/filter/RbacGatewaySentinelFilter.java
@@ -0,0 +1,99 @@
+package com.xhh.rbac.gateway.filter;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.SentinelGatewayConstants;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPathPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayParamFlowItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.ZuulBlockFallbackManager;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulErrorFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulPostFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulPreFilter;
+import com.netflix.zuul.ZuulFilter;
+import com.xhh.rbac.gateway.fallback.RbacGatewayBlockFallbackProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+GatewayFlowRule：网关限流规则，针对 API Gateway 的场景定制的限流规则，可以针对不同 route 或自定义的 API 分组进行限流，支持针对请求中的参数、Header、来源 IP 等进行定制化的限流。
+ApiDefinition：用户自定义的 API 定义分组，可以看做是一些 URL 匹配的组合。比如我们可以定义一个 API 叫 my_api，请求 path 模式为 /foo/** 和 /baz/** 的都归到 my_api 这个 API 分组下面。限流的时候可以针对这个自定义的 API 分组维度进行限流。
+回头看initGatewayRules方法，我们通过ApiDefinition定义了一个API分组，名称为captcha，匹配的URL为/auth/captcha；然后通过GatewayFlowRule指定了限流的规则。其中网关限流规则 GatewayFlowRule 的字段解释如下：
+
+resource：资源名称，可以是网关中的 route 名称或者用户自定义的 API 分组名称。
+resourceMode：规则是针对 API Gateway 的 route（RESOURCE_MODE_ROUTE_ID）还是用户在 Sentinel 中定义的 API 分组（RESOURCE_MODE_CUSTOM_API_NAME），默认是 route。
+grade：限流指标维度，同限流规则的 grade 字段。
+count：限流阈值
+intervalSec：统计时间窗口，单位是秒，默认是 1 秒。
+controlBehavior：流量整形的控制效果，同限流规则的 controlBehavior 字段，目前支持快速失败和匀速排队两种模式，默认是快速失败。
+burst：应对突发请求时额外允许的请求数目。
+maxQueueingTimeoutMs：匀速排队模式下的最长排队时间，单位是毫秒，仅在匀速排队模式下生效。
+paramItem：参数限流配置。若不提供，则代表不针对参数进行限流，该网关规则将会被转换成普通流控规则；否则会转换成热点规则。其中的字段：
+parseStrategy：从请求中提取参数的策略，目前支持提取来源 IP（PARAM_PARSE_STRATEGY_CLIENT_IP）、Host（PARAM_PARSE_STRATEGY_HOST）、任意 Header（PARAM_PARSE_STRATEGY_HEADER）和任意 URL 参数（PARAM_PARSE_STRATEGY_URL_PARAM）四种模式。
+fieldName：若提取策略选择 Header 模式或 URL 参数模式，则需要指定对应的 header 名称或 URL 参数名称。
+pattern：参数值的匹配模式，只有匹配该模式的请求属性值会纳入统计和流控；若为空则统计该请求属性的所有值。（1.6.2 版本开始支持）
+matchStrategy：参数值的匹配策略，目前支持精确匹配（PARAM_MATCH_STRATEGY_EXACT）、子串匹配（PARAM_MATCH_STRATEGY_CONTAINS）和正则匹配（PARAM_MATCH_STRATEGY_REGEX）。（1.6.2 版本开始支持）
+*/
+@Slf4j
+@Configuration
+public class RbacGatewaySentinelFilter {
+
+    @Bean
+    public ZuulFilter sentinelZuulPreFilter() {
+        return new SentinelZuulPreFilter();
+    }
+
+    @Bean
+    public ZuulFilter sentinelZuulPostFilter() {
+        return new SentinelZuulPostFilter();
+    }
+
+    @Bean
+    public ZuulFilter sentinelZuulErrorFilter() {
+        return new SentinelZuulErrorFilter();
+    }
+
+    @PostConstruct
+    public void doInit() {
+        ZuulBlockFallbackManager.registerProvider(new RbacGatewayBlockFallbackProvider());
+        initGatewayRules();
+    }
+
+    /**
+     * 定义验证码请求限流，限流规则：
+     * 60秒内同一个IP，同一个 key最多访问 10次
+     */
+    private void initGatewayRules() {
+        Set<ApiDefinition> definitions = new HashSet<>();
+        Set<ApiPredicateItem> predicateItems = new HashSet<>();
+
+        predicateItems.add(new ApiPathPredicateItem().setPattern("/auth/captcha"));
+        ApiDefinition definition = new ApiDefinition("captcha")
+                .setPredicateItems(predicateItems);
+        definitions.add(definition);
+        GatewayApiDefinitionManager.loadApiDefinitions(definitions);
+
+        Set<GatewayFlowRule> rules = new HashSet<>();
+
+        rules.add(new GatewayFlowRule("captcha")
+                .setResourceMode(SentinelGatewayConstants.RESOURCE_MODE_CUSTOM_API_NAME)
+                .setParamItem(
+                        new GatewayParamFlowItem()
+                                .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_URL_PARAM)
+                                .setFieldName("key")
+                                .setMatchStrategy(SentinelGatewayConstants.PARAM_MATCH_STRATEGY_EXACT)
+                                .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_CLIENT_IP)
+                )
+                .setCount(10)
+                .setIntervalSec(60)
+        );
+        GatewayRuleManager.loadRules(rules);
+    }
+}
